a090ae8348d441ce212bc60bdc2d6f942aa36e88
kiang <kiange@gmail.com>
2024-12-28 22:12:59 +0800

    use curl to fetch data

--- scripts/02_csv2geojson.php
+++ scripts/02_csv2geojson.php
@@ -590,8 +590,27 @@ while ($row = fgetcsv($fh)) {
         $tokenPage = $browser->getResponse()->getContent();
         $tokenParts = explode('"', $tokenPage);
 
-        $browser->request('GET', "https://easymap.land.moi.gov.tw/Z10Web/Land_json_locate?sectNo={$section['id']}&office={$section['officeCode']}&landNo={$data['地號']}123123&struts.token.name={$tokenParts[9]}&token={$tokenParts[11]}");
-        $result = $browser->getResponse()->getContent();
+        $result = exec("curl 'https://easymap.land.moi.gov.tw/W10Web/Land_json_locate' \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Connection: keep-alive' \
+  -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
+  -H 'Origin: https://easymap.land.moi.gov.tw' \
+  -H 'Pragma: no-cache' \
+  -H 'Referer: https://easymap.land.moi.gov.tw/W10Web/Normal' \
+  -H 'Sec-Fetch-Dest: empty' \
+  -H 'Sec-Fetch-Mode: cors' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H 'sec-ch-ua: \"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: \"Linux\"' \
+  --data-raw 'sectNo={$section['id']}&office={$section['officeCode']}&landNo={$data['地號']}&struts.token.name={$tokenParts[9]}&token={$tokenParts[11]}'");
+
+        // $browser->request('GET', "https://easymap.land.moi.gov.tw/Z10Web/Land_json_locate?sectNo={$section['id']}&office={$section['officeCode']}&landNo={$data['地號']}&struts.token.name={$tokenParts[9]}&token={$tokenParts[11]}");
+        // $result = $browser->getResponse()->getContent();
 
         while (false !== strpos($result, '系統檢測您的連線不正常')) {
             echo $result;
@@ -599,9 +618,24 @@ while ($row = fgetcsv($fh)) {
             $browser->request('GET', 'https://easymap.land.moi.gov.tw/Z10Web/layout/setToken.jsp');
             $tokenPage = $browser->getResponse()->getContent();
             $tokenParts = explode('"', $tokenPage);
-
-            $browser->request('GET', "https://easymap.land.moi.gov.tw/Z10Web/Land_json_locate?sectNo={$section['id']}&office={$section['officeCode']}&landNo={$data['地號']}123123&struts.token.name={$tokenParts[9]}&token={$tokenParts[11]}");
-            $result = $browser->getResponse()->getContent();
+            $result = exec("curl 'https://easymap.land.moi.gov.tw/W10Web/Land_json_locate' \
+            -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+            -H 'Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7' \
+            -H 'Cache-Control: no-cache' \
+            -H 'Connection: keep-alive' \
+            -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
+            -H 'Origin: https://easymap.land.moi.gov.tw' \
+            -H 'Pragma: no-cache' \
+            -H 'Referer: https://easymap.land.moi.gov.tw/W10Web/Normal' \
+            -H 'Sec-Fetch-Dest: empty' \
+            -H 'Sec-Fetch-Mode: cors' \
+            -H 'Sec-Fetch-Site: same-origin' \
+            -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36' \
+            -H 'X-Requested-With: XMLHttpRequest' \
+            -H 'sec-ch-ua: \"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"' \
+            -H 'sec-ch-ua-mobile: ?0' \
+            -H 'sec-ch-ua-platform: \"Linux\"' \
+            --data-raw 'sectNo={$section['id']}&office={$section['officeCode']}&landNo={$data['地號']}&struts.token.name={$tokenParts[9]}&token={$tokenParts[11]}'");
         }
 
         if (false === strpos($result, '地號查詢無資料')) {
